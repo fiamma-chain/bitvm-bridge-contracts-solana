6b86ec1df53ee65b056b75d046ff3ccb79f8fbf7
Ubuntu <ubuntu@ip-172-31-25-71.ap-northeast-1.compute.internal>
2024-11-22 03:15:58 +0000

    add some test

--- tests/bitvm-bridge-contracts-solana.ts
+++ tests/bitvm-bridge-contracts-solana.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { Keypair } from '@solana/web3.js';
 import { BitvmBridgeContractsSolana } from "../target/types/bitvm_bridge_contracts_solana";
 
 describe("bitvm-bridge-contracts-solana", () => {
+
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
+
+  const payer = provider.wallet as anchor.Wallet;
 
   const program = anchor.workspace.BitvmBridgeContractsSolana as Program<BitvmBridgeContractsSolana>;
 
-  it("Is initialized!", async () => {
+  const metadata = {
+    name: 'Solana Gold',
+    symbol: 'GOLDSOL',
+    uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+  };
+
+  // Generate new keypair to use as address for mint account.
+  const mintKeypair = new Keypair();
+
+  it("Initialize an SPL Token", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const transactionSignature = await program.methods.initialize(metadata.name, metadata.symbol, metadata.uri).accounts({
+      payer: payer.publicKey,
+      mintAccount: mintKeypair.publicKey,
+    }).signers([mintKeypair]).rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
   });
+
 });
